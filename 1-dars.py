4ec846e76b31f6675a89a07e0c04c52149e7f3ff
Xalilov <eljjey@gmail.com>
2021-01-27 19:33:47 +0500

    1-darscha

--- 1-dars.py
+++ 1-dars.py
@@ -1,4 +1,9 @@
-num_one=5
-num_two=4
-sum=num_one+num_two
-print(sum)
+def nam1():
+    num_one=5
+    num_two=4
+    sum=num_one+num_two
+    return sum
+
+nam1()
+f=open('num1.txt','w')
+f.write(str(nam1()))
